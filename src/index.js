36af4087039646c8e116d6347578ebde167cc1df
JaviPeder~ <javierpedernera@gmail.com>
2023-01-23 18:14:00 -0300

    DeployFront

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Store from "./store/index.js"
 import axios from 'axios';
 
 // axios.defaults.baseURL= 'http://localhost:3001';
-axios.defaults.baseURL= 'https://countriesback-production.up.railway.app/countries';
+axios.defaults.baseURL= 'https://countriesback-production.up.railway.app';
 
 ReactDOM.render(
   <Provider store={Store}>
